6db9d3b5719e2bfc805cf625da44b2a532d8a21f
0xAshkan <0x6173686b@gmail.com>
2023-08-18 17:16:17 +0330

    update event resolver

--- assets/js/upcoming-events.js
+++ assets/js/upcoming-events.js
@@ -1,54 +1,51 @@
 // Function to fetch upcoming events from CTFtime API
 async function fetchUpcomingEvents() {
-    try {
-      const response = await fetch(window.origin+'/upcomings.txt');
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      return [];
-    }
+  try {
+    const response = await fetch(window.origin+'/upcomings.txt');
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return [];
   }
-  
-  // Function to generate event cards
-  function generateEventCard(event) {
-    const eventCard = document.createElement('div');
-    eventCard.classList.add('event-card');
-  
-    const eventDate = document.createElement('p');
-    eventDate.classList.add('event-date');
-    eventDate.textContent = new Date(event.start).toLocaleDateString('en-US', {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-    });
-  
-    const eventTitle = document.createElement('h3');
-    eventTitle.textContent = event.title;
-  
-    const eventDescription = document.createElement('p');
-    eventDescription.textContent = event.format + ' - ' + event.onsite;
-  
-    eventCard.appendChild(eventDate);
-    eventCard.appendChild(eventTitle);
-    eventCard.appendChild(eventDescription);
-  
-    return eventCard;
-  }
-  
-  // Function to add events to the DOM
-  function displayUpcomingEvents(events) {
-    const upcomingEventsContainer = document.getElementById('upcoming-events');
-  
-    events.forEach(event => {
-      const eventCard = generateEventCard(event);
-      upcomingEventsContainer.appendChild(eventCard);
-    });
-  }
-  
-  // Fetch and display upcoming events
-  fetchUpcomingEvents()
-    .then(events => {
-      displayUpcomingEvents(events);
-    });
-  
+}
+
+// generate event rows in a table
+function generateEventRow(event) {
+  const tableRow = document.createElement('tr');
+
+  const eventDate = document.createElement('td');
+  eventDate.textContent = new Date(event.start).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+
+  const eventTitle = document.createElement('td');
+  eventTitle.textContent = event.title;
+
+  const eventDescription = document.createElement('td');
+  eventDescription.textContent = event.format + ' - ' + event.onsite;
+
+  tableRow.appendChild(eventDate);
+  tableRow.appendChild(eventTitle);
+  tableRow.appendChild(eventDescription);
+
+  return tableRow;
+}
+
+// add events to the table
+function displayUpcomingEvents(events) {
+  const tableBody = document.getElementById('upcoming-events-table-body');
+
+  events.forEach(event => {
+    const tableRow = generateEventRow(event);
+    tableBody.appendChild(tableRow);
+  });
+}
+
+// Fetch and display upcoming events
+fetchUpcomingEvents()
+  .then(events => {
+    displayUpcomingEvents(events);
+  });
