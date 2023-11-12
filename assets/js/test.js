1db782cc925833430c4136c8d71f3beee1c18c7d
0xAshkan <0x6173686b@gmail.com>
2023-11-12 11:44:00 +0330

    edit js

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,4 +1,4 @@
-var myDiv = document.getElementById("div")
-var newParagraph = document.createElement("p")
-newParagraph.textContent = "This is just for test"
-myDiv.appendChild(newParagraph)
+var myDiv = document.getElementById("div");
+var newParagraph = document.createElement("p");
+newParagraph.textContent = "This is just for test";
+myDiv.appendChild(newParagraph);
